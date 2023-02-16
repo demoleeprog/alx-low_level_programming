df1c4b47def5ae94ca50fb5e9f7979a46dc134ff
Bello Yusuf <demolawrites@gmail.com>
2023-02-16 01:21:55 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line using puts function
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle
+");
+return (0);
+}
